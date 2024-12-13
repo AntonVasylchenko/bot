5f52373fc78854a9fd604f56ee96b11c6d41b3b4
Anton <anton@Antons-MacBook-Pro.local>
2024-12-13 20:52:17 +0200

    start

---
+++ index.js
@@ -0,0 +1,7 @@
+const Binance = require('binance-api-node').default;
+
+const client = Binance({
+  apiKey: 'K5MCdR8BX4gNLsryObwAtWvBxA2wUI3MklVJZs4jrjnFaIN4NM1H16mxL51TLD9S', // Введіть свій API Key
+  apiSecret: 'xHpw6kw64iTkehTZUPH66DIYkwJQrQXeceUNsywgdvYC7IJBAoM5kzxSPg09PWvg', // Введіть свій Secret Key
+});
+
